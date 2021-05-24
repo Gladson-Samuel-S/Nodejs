7ace947325875bcea86328260b8baef0c3ffd188
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-05-24 12:34:03 +0530

    Using streams to read and write data :electric_plug:

---
+++ stream.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/Welcome2.txt', {encoding: 'utf-8'})
+const writeStream = fs.createWriteStream('./docs/Welcom3.txt')
+
+readStream.on('data', (chunk) => {
+    console.log('\n------New Chunk------\n')
+    console.log(chunk)
+    writeStream.write('\nNew Chunk\n')
+    writeStream.write(chunk)
+})
+
+// pipe
+readStream.pipe(writeStream)
