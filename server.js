0a21bc8489a99f89756589db0a401de6d10153a4
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-05-26 12:49:37 +0530

    Created my first server :tada:

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Gladson Samuel')
+// Creating a Server
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('request made')
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000')
+})
+
